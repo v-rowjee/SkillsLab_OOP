fbd623f4537393afb58e5dcc496d989ef6075032
v-rowjee <vedrowjee2001@gmail.com>
2023-11-12 16:43:54 +0400

    Enums

---
+++ SkillsLab_OOP/Enums/RoleEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillsLab_OOP.Enums
+{
+    public enum RoleEnum
+    {
+        Employee = 0,
+        Manager = 1,
+        Admin = 2
+    }
+}
