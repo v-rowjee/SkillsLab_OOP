e74452b189095c35cbd66b51202ac88f265b15c8
v-rowjee <vedrowjee2001@gmail.com>
2023-11-12 16:43:32 +0400

    Use of generics

---
+++ SkillsLab_OOP/DAL/Common/IDAL.cs
@@ -0,0 +1,18 @@
+﻿using SkillsLab_OOP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillsLab_OOP.DAL.Common
+{
+    public interface IDAL<T>
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        bool Add(T model);
+        T Update(T model);
+        bool Delete(int id);
+    }
+}
