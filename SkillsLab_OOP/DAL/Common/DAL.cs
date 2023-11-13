02b7037e3dda2b400312598c34fc8705bad8ec60
v-rowjee <vedrowjee2001@gmail.com>
2023-11-13 16:47:23 +0400

    connection string

--- SkillsLab_OOP/DAL/Common/DAL.cs
+++ SkillsLab_OOP/DAL/Common/DAL.cs
@@ -8,14 +8,14 @@ namespace SkillsLab_OOP.DAL.Common
 {
     public class DAL
     {
-        //public const string connectionString = @"server=L-PC197T2Z;database=JEDICarpool_db;uid=wbpoc;pwd=sql@tfs2008";
-        public const string connectionString = @"server=jedicarpooldbserver.database.windows.net;database=JEDICarpool_db;uid=wbpoc;pwd=sql@tfs2008";
+        // public const string connectionString = @"server=jedicarpooldbserver.database.windows.net;database=JEDICarpool_db;uid=wbpoc;pwd=sql@tfs2008";
+        public const string CONNECTION_STRING = @"server=localhost;database=SkillsLabDB;";
 
         public SqlConnection Connection;
 
         public DAL()
         {
-            Connection = new SqlConnection(connectionString);
+            Connection = new SqlConnection(CONNECTION_STRING);
             OpenConnection();
         }
 
