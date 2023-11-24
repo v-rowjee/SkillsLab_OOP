199c9e2cf9942f3bd0136e48338aa6ff187e3afa
v-rowjee <vedrowjee2001@gmail.com>
2023-11-24 15:59:20 +0400

    dal

---
+++ SkillsLabConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using SkillsLab_OOP;
+using SkillsLab_OOP.DAL;
+using SkillsLab_OOP.DAL.Common;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        TrainingDAL trainingDAL = new TrainingDAL();
+        var trainings = trainingDAL.GetAllTrainings().ToList();
+        trainings.ForEach(x => { Console.WriteLine(x); });
+    }
+}
